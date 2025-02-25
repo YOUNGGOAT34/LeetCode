bb1d2dbe184f3f5f9f034ca537323cac7d988b58
GOAT <goat@MiWiFi-R4CM-srv.GOAT>
2025-02-25 16:44:15 +0300

    number of sub-arrays with odd sum

---
+++ Daily challenge/number_of_sub_arrays_with_odd_sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numOfSubarrays(self, arr: List[int]) -> int:
+        curr_sum=0# current sum
+        odd_freq=0 # the odd sums encountered so far
+        even_freq=0 # even sums encountered so far
+        res=0
+        MOD=10**9+7
+
+        for n in arr:
+            curr_sum+=n
+            if curr_sum&1:
+                res=(res+even_freq+1)%MOD
+                odd_freq+=1
+
+            else:
+                res=(res+odd_freq)%MOD
+                even_freq+=1
+
+        return res
+
+        
