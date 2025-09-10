e6299c8d571cde03011a5b5f80a10a29756aa7eb
GOAT <lucasonyiego283@gmail.com>
2025-09-10 18:17:46 +0300

    product of all then find each ith product by dividing by nums[i] is efficient but encounters division by zero error if the ith element is a 0

--- algo/product_of_array_except_self.c
+++ algo/product_of_array_except_self.c
@@ -1,26 +1,54 @@
 #include<stdio.h>
 
 //brute force solution;
+// int* productExceptSelf(int* nums, int numsSize, int* returnSize) {
+      
+//    int *ret=malloc(sizeof(int)*numsSize);
+//    *returnSize=0;
+
+//    for(int i=0;i<numsSize;i++){
+//        int prod=1;
+//        for(int j=0;j<numsSize;j++){
+//            if(i==j){
+//                continue;
+//            }
+         
+//           prod*=nums[j];
+          
+
+//        }
+
+//        ret[*returnSize]=prod;
+//        (*returnSize)++;
+//    }
+
+//    return ret;
+// }
+
+
+//total product then divide by the sel
+
 int* productExceptSelf(int* nums, int numsSize, int* returnSize) {
       
    int *ret=malloc(sizeof(int)*numsSize);
+ //   int *prefix=malloc(sizeof(int)*numsSize);
+ //   int *suffix=malloc(sizeof(int)*numsSize);
    *returnSize=0;
+  int prod=1;
 
-   for(int i=0;i<numsSize;i++){
-       int prod=1;
-       for(int j=0;j<numsSize;j++){
-           if(i==j){
-               continue;
-           }
-         
-          prod*=nums[j];
-          
+    for(int i=0;i<numsSize;i++){
+        prod*=nums[i];
+    }
 
-       }
+    for(int i=0;i<numsSize;i++){
+         ret[*returnSize]=prod/nums[i];
 
-       ret[*returnSize]=prod;
-       (*returnSize)++;
-   }
+         *returnSize=i;
+    }
 
+ //   memset() 
+
+ //   free(prefix);
+ //   free(suffix);
    return ret;
 }
