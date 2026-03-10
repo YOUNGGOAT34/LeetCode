e1c39776c5a83f8b54fba536e8ad7b21c78ecaa2
GOAT <lucasonyiego283@gmail.com>
2026-03-10 23:09:39 +0300

    sum of left leaves

---
+++ bt/sum_of_left_leaves.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        
+        total=0
+
+        def dfs(node):
+            nonlocal total
+            if not node:
+                return
+
+            dfs(node.left)
+            dfs(node.right)
+
+            if node.left and not node.left.left and not node.left.right:
+                total+=node.left.val
+
+            return
+
+        dfs(root)
+        return total
+
+
+            
+
+        
