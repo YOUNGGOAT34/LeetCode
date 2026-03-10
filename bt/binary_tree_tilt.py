0b59e2bedfce1a3585a16832c4fef311eb743510
GOAT <lucasonyiego283@gmail.com>
2026-03-10 22:45:16 +0300

    binary tree tilt

---
+++ bt/binary_tree_tilt.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findTilt(self, root: Optional[TreeNode]) -> int:
+        total=0
+        def dfs(node)->int:
+            nonlocal total
+            if not node:
+                return 0
+
+            left=dfs(node.left)
+            right=dfs(node.right)
+
+            total+=abs(left-right)
+
+            return left+right+node.val
+
+        dfs(root)
+
+        return total
+        
