8268c2bb9a48257a490581505cacea0ad06fb7bc
GOAT <lucasonyiego283@gmail.com>
2026-04-15 22:30:56 +0300

    two sum sorted

---
+++ two_pointers/two_sum.py
@@ -0,0 +1,20 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        left=0
+        right=len(nums)-1
+
+        while left<=right:
+
+            ans=nums[left]+nums[right]
+
+            if ans==target:
+                return [left+1,right+1]
+
+            elif ans>target:
+                right-=1
+
+            else:
+                left+=1
+
+        
